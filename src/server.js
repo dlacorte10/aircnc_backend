987c720f9ac7dd705b64732533aa1883b2d550d5
Daniel Lacorte <daniel.lacorte10@gmail.com>
2019-10-04 11:13:36 -0300

    Acesso ao Banco do Mongo DB Atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-tlrkz.mongodb.net/semana09?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
